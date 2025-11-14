28a597f97fb23d93b9c757f388d266470d849561
Veronika Kulynych <kulynychron@gmail.com>
2025-11-14 11:05:53 +0200

    ready hm

---
+++ src/models/note.js
@@ -0,0 +1,30 @@
+import { model } from 'mongoose';
+
+import { Schema } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true, // прибирає пробіли на початку та в кінці
+    },
+    content: {
+      type: String,
+     default: '',
+      trim: true,
+    },
+    tag: {
+      type: String,
+      enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', noteSchema);
+
--- src/server.js
+++ src/server.js
@@ -1,64 +1,29 @@
 import 'dotenv/config';
-import express from 'express';
 import cors from 'cors';
-import pinoHttp from 'pino-http';
+import express from 'express';
 
+import { connectMongoDB } from './db/connectMongoDB.js';
 
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(express.json());
-app.use(cors());
-app.use(
-  pinoHttp({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger);         // 1. Логер першим — бачить усі запити
+app.use(express.json()); // 2. Парсинг JSON-тіла
+app.use(cors());         // 3. Дозвіл для запитів з інших доменів
+app.use(notesRoutes);
 
 
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
 
-app.get('/notes/:noteId', (req, res) => {
-   const id_param = req.params.noteId;
-  res.status(200).json({ message: `Retrieved note with ID: ${id_param}` });
-});
-
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
-// Middleware 404 (після всіх маршрутів)
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// Middleware для обробки помилок (останнє)
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  const isProd = process.env.NODE_ENV === "production";
-
-  res.status(500).json({
-    message: isProd
-      ? "Something went wrong. Please try again later."
-      : err.message,
-  });
-});
+app.use(notFoundHandler);
+// Error — якщо під час запиту виникла помилка
+app.use(errorHandler);
+await connectMongoDB();
 
 // Запуск сервера
 app.listen(PORT, () => {
