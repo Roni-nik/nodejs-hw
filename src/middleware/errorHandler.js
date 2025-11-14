3361f62888bafd7983f579664295c33a67a8eed8
Veronika Kulynych <kulynychron@gmail.com>
2025-11-14 11:44:23 +0200

    fix

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -5,6 +5,7 @@ export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     return res.status(err.status).json({
       message: err.message || err.name,
+      
     });
   }
 
